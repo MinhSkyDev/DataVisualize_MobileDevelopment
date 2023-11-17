2ae55d77e0f4c0f48d697f985be91d1aaff86168
Erel <tranducanhghu@gmail.com>
2023-11-17 19:26:21 +0700

    type of data for chart

--- code/ReactDataVisual/src/constants/data.js
+++ code/ReactDataVisual/src/constants/data.js
@@ -7,14 +7,47 @@ export const xRotation = [
   { label: "Employment Status",  value: 'EmploymentStatus' },
   { label: 'Department',         value: 'Department' },
   { label: 'Recruitment Source', value: 'RecruitmentSource' },
+  { label: 'Citizen',            value: 'CitizenDesc' },
+  { label: 'Marital',            value: 'MaritalDesc' },
+  { label: 'Performance Score',  value: 'PerformanceScore' },
+  { label: 'Race',               value: 'RaceDesc' },
+  { label: 'Sex',                value: 'Sex' },
+  //{ label: 'Absences',           value: 'Absences' },
 ]
 
 export const yRotation = [
-  { label: "Salary",                 value: 'Salary' },
-  { label: "Days Late Last 30",      value: 'DaysLateLast30' },
-  { label: 'Special Projects Count', value: 'SpecialProjectsCount' },
-  { label: 'Absences',               value: 'Absences' },
+  { label: 'Absences',                    value: 'Absences' },
+  { label: "Salary",                      value: 'Salary' },
+  { label: "Days Late Last 30",           value: 'DaysLateLast30' },
+  { label: 'Special Projects Count',      value: 'SpecialProjectsCount' },
+  { label: 'Performance Score',           value: 'PerfScoreID' },
+  { label: 'Emsatisfaction',              value: 'Emsatisfaction' },
+  { label: 'Engagement Survey',           value: 'EngagementSurvey' },
+  /* Doable but hard af
+  { label: 'Performance Score - Sum',     value: 'PerformanceScore' },
+  { label: 'Citizen',                     value: 'CitizenDesc' },
+  { label: 'Marital',                     value: 'MaritalDesc' },
+  { label: 'Sex',                         value: 'Sex' },
+  { label: 'Department',                  value: 'Department' },
+  { label: 'Employment Status',           value: 'EmploymentStatus' },
+  */
 ]
+
+/* report data x depend on y value
+export const sumAble = [
+  "Absences",
+  "Salary",
+  "DaysLateLast30",
+  "SpecialProjectsCount",
+  "PerfScoreID",
+]
+
+export const avgAble = [
+  "PerfScoreID",
+  "Emsatisfaction", 
+  "EngagementSurvey",
+]
+*/
 
 export const typeChart = [
   { label: 'Line Chart', value: 'LineChart' },
